20f8b9128ea95e12d7a17fbcb2ef1931b8e4fa69
tm <tm@tm.cz>
2022-06-07 11:52:12 +0200

    backend - detail

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('users/', views.UserListView.as_view(), name='users'),
-    path('user/<int:pk>', views.UserDetailView.as_view(), name='user-detail'),
+    path('user/<int:pk>', views.UserDetailView.as_view(), name='userDetail'),
     path('rooms/', views.RoomListView.as_view(), name='rooms'),
-    path('room/<int:pk>', views.RoomDetailView.as_view(), name='room-detail'),
+    path('room/<int:pk>', views.RoomDetailView.as_view(), name='roomDetail'),
     path('messages/', views.MessageListView.as_view(), name='messages'),
-    path('message/<int:pk>', views.MessageDetailView.as_view(), name='message-detail'),
+    path('message/<int:pk>', views.MessageDetailView.as_view(), name='messageDetail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -25,27 +25,33 @@ def index(request):
 class UserListView(generic.ListView):
     model = User
     context_object_name = 'user_list'
-    queryset = User.objects.filter(name__icontains='a')[:5]
-    template_name = 'users/users_t_list.html'
+    # queryset = User.objects.filter(name__icontains='a')[:5]
+    template_name = 'users/user_list.html'
 
 class UserDetailView(generic.DetailView):
     model = User
+    context_object_name = 'userDetail'
+    template_name = 'catalog/user_detail.html'
 
 
 class RoomListView(generic.ListView):
     model = Room
     context_object_name = 'room_list'
-    queryset = Room.objects.filter(title__icontains='a')[:5]
-    template_name = 'rooms/rooms_t_list.html'
+    # queryset = Room.objects.filter(title__icontains='a')[:5]
+    template_name = 'rooms/room_list.html'
 
 class RoomDetailView(generic.DetailView):
     model = Room
+    context_object_name = 'roomDetail'
+    template_name = 'room_detail.html'
 
 class MessageListView(generic.ListView):
     model = Message
     context_object_name = 'message_list'
-    queryset = Message.objects.filter(title__icontains='a')[:5]
-    template_name = 'messages/messages_t_list.html'
+    # queryset = Message.objects.filter(title__icontains='a')[:5]
+    template_name = 'messages/message_list.html'
 
 class MessageDetailView(generic.DetailView):
     model = Message
+    context_object_name = 'messageDetail'
+    template_name = 'message_detail.html'
